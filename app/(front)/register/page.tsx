b85449997c6d4312cb8223bbfcda73e6b65748a1
DzulhiRaihan <dzulhiraihan@gmail.com>
2024-05-30 17:28:11 +0800

    updated login, register and forgot password page

--- app/(front)/register/page.tsx
+++ app/(front)/register/page.tsx
@@ -1,52 +1,61 @@
 "use client";
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function Register( ) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#1F2128]-100">
-      <div className="w-full max-w-md p-6 bg-[#1F2128] rounded-3xl shadow-md mt-[-150px]">
-        <h2 className="text-2xl font-bold text-center text-white mb-4">Register</h2>
-        <form>
-          <div className="mb-4">
-            <label className="block text-white">Email</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-2 rounded bg-gray-200"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-white">Password</label>
+    <div className="container mx-auto my-auto">
+      <div className="bg-[#1F2128] rounded-lg shadow-md w-3/4 mx-auto py-10 my-4">
+        <div className="flex flex-row mx-auto justify-center pb-8">
+          <p className="text-6xl font-bold">Welcome To Quiz</p>
+          <p className="text-3xl pt-[10px] pl-1 text-purple-400">tion</p>
+        </div>
+        <div className="w-2/4 mx-auto bg-gray-900 p-6 rounded-lg">
+          <h2 className="text-2xl font-bold text-center text-white mb-4">
+            Register
+          </h2>
+          <form>
+            <div className="mb-3">
+              <label className="block text-white pb-1">Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full p-2 rounded bg-gray-700 focus:outline-none focus:border-black focus:ring-black focus:outline-gray-600"
+                required
+              />
+            </div>
+            <div className="mb-3">
+            <label className="block text-white pb-1">Password</label>
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 rounded bg-gray-200"
+              className="w-full p-2 rounded bg-gray-700 focus:outline-none focus:border-black focus:ring-black focus:outline-gray-600"
               required
             />
           </div>
-          <div className="mb-4">
-            <label className="block text-white">Confirm Password</label>
+          <div className="mb-5">
+            <label className="block text-white pb-1">Confirm Password</label>
             <input
               type="password"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
-              className="w-full p-2 rounded bg-gray-200"
+              className="w-full p-2 rounded bg-gray-700 focus:outline-none focus:border-black focus:ring-black focus:outline-gray-600"
               required
             />
           </div>
-          <button type="submit" className="w-full p-2 bg-gradient-to-r from-purple-500 to-blue-500 text-white rounded">Register</button>
-        </form>
-        <div className="flex justify-center mt-4 text-white">
-          <button  className="underline">Back to Login</button>
+            <button
+              type="submit"
+              className="w-full p-2 bg-gradient-to-r from-purple-500 to-blue-500 text-white rounded"
+            >
+              Register
+            </button>
+          </form>
+        
         </div>
       </div>
     </div>
